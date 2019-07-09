bb359e21c5c40214d7750785e2e92859c2237e86
Victor Ezlien-Taylor <35602856+vickydesh@users.noreply.github.com>
2019-07-09 07:43:26 -0400

    Camara

---
+++ script.js
@@ -0,0 +1,23 @@
+const videoPlayer = document.querySelector('#player');
+const canvasElement = document.querySelector('#canvas');
+const captureButton = document.querySelector('#capture-btn');
+const imagePicker = document.querySelector('#image-picker');
+const imagePickerArea = document.querySelector('#pick-image');
+const newImages = document.querySelector('#newImages');
+
+// Image dimensions
+const width = 320;
+const height = 240;
+let   zIndex = 1;
+
+
+const startMedia = () => {
+    // Play the video if possible
+};
+
+// Capture the image, save it and then show it in the page
+captureButton.addEventListener('click', (event) => {
+
+});
+
+window.addEventListener("load", (event) => startMedia());
